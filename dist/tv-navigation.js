6768adf10f5a1cbf02878d007021636cfd2b35a5
Vitor Cavalcanti <vitor.cavalcanti@corp.globo.com>
2020-01-07 10:51:07 -0300

    chore(dist): add modified dist files

--- dist/tv-navigation.js
+++ dist/tv-navigation.js
@@ -1,5 +1,3 @@
-
-(function(l, r) { if (l.getElementById('livereloadscript')) return; r = l.createElement('script'); r.async = 1; r.src = '//' + (window.location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; r.id = 'livereloadscript'; l.head.appendChild(r) })(window.document);
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
@@ -1062,7 +1060,9 @@
     this.destroy();
   };
 
-  var index = new TVNavigation();
+  var TVNavigation$1 = new TVNavigation();
+
+  var index = { TVNavigation: TVNavigation$1 };
 
   return index;
 
