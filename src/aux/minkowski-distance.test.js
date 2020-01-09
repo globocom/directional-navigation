35640227a5e1fb21347f1a7c3e0f15423dcf8bcb
Vitor Cavalcanti <vitor.cavalcanti@corp.globo.com>
2020-01-09 20:24:32 -0300

    chore(test): add some tests

--- src/aux/minkowski-distance.test.js
+++ src/aux/minkowski-distance.test.js
@@ -11,10 +11,19 @@ describe('MinkowskiDistance', () => {
 
       expect(distance).toBe(1)
     })
+
+    it('should return the xy distance between the source and target point without order', () => {
+      const source = [0, 1]
+      const target = [1, 1]
+
+      const distance = MinkowskiDistance.calculate(source, target)
+
+      expect(distance).toBe(1)
+    })
   })
 
   describe('on calculating distance using chebyshev distance', () => {
-    it('should return the xy distance between the source and target point', () => {
+    it('should return the xy distance using Number.POSITIVE_INFINITY as order', () => {
       const source = [0, 1]
       const target = [3, 3]
       const order = Number.POSITIVE_INFINITY
@@ -22,6 +31,41 @@ describe('MinkowskiDistance', () => {
       const distance = MinkowskiDistance.calculate(source, target, order)
 
       expect(distance).toBe(3)
+    })
+
+    it('should return the xy distance using Number.NEGATIVE_INFINITY as order', () => {
+      const source = [0, 1]
+      const target = [3, 3]
+      const order = Number.NEGATIVE_INFINITY
+
+      const distance = MinkowskiDistance.calculate(source, target, order)
+
+      expect(distance).toBe(2)
+    })
+  })
+  describe('on calculating distance using improper params', () => {
+    it('should throw an error when using improper number of points', () => {
+      const source = [0]
+      const target = [1, 1]
+      const order = 2
+
+      expect(() => MinkowskiDistance.calculate(source, target, order)).toThrowError()
+    })
+
+    it('should throw an error when using a negative order other than Number.NEGATIVE_INFINITY', () => {
+      const source = [0, 1]
+      const target = [1, 1]
+      const order = -1
+
+      expect(() => MinkowskiDistance.calculate(source, target, order)).toThrowError()
+    })
+
+    it('should throw an error when using a NaN order', () => {
+      const source = [0, 1]
+      const target = [1, 1]
+      const order = 'order'
+
+      expect(() => MinkowskiDistance.calculate(source, target, order)).toThrowError()
     })
   })
 })
