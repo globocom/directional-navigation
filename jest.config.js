9c6df6dcc2725e22a6cbbc0782f76dc491d79616
Vitor Cavalcanti <vitor.cavalcanti@corp.globo.com>
2020-01-14 13:00:57 -0300

    chore(jest): add coverage reports config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   testURL: 'http://localhost',
   moduleNameMapper: { 'clappr-zepto': '<rootDir>node_modules/clappr-zepto/zepto.js' },
+  coverageReporters: ['text-summary', 'lcov'],
+  collectCoverageFrom: [
+    '**/src/**/*.{js}',
+    '!**/*.test.*',
+    '!**/*.spec.*',
+  ],
 }
